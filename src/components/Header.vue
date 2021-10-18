65399c02879483f37eb52bb1adf0fb4cf5a09e4e
haopengsun <sunhaopeng2016@gmail.com>
2021-10-18 18:32:55 -0400

    add title header button

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <div class="btn">
+            <Button v-on:click="onclick()" text="Add" color="green"/>
+        </div>
+    </header>
+</template>
+
+<script>
+import Button from "./Button.vue"
+
+export default {
+    name: 'Header',
+    props: {
+        title: {
+            type: String,
+            default: "Hello World",
+        },
+    },
+    components: {
+        Button
+    },
+    methods: {
+        onclick(){
+            console.log('click')
+        }
+    }
+}
+</script>
+
+<style scoped>
+    header{
+        display: flex;
+        flex-direction: column;
+    }
+</style>
