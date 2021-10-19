4ad2a6cef0e3321edfdd28c97bf11a079206ce8d
haopengsun <sunhaopeng2016@gmail.com>
2021-10-18 21:53:31 -0400

    add toggle reminder and delete item functions

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,56 @@
 <template>
   <div class="container">
     <Header title="Task Tracker"/>
+    <Tasks @delete-task="deleteTask" @toggle-reminder="toggleReminder" :tasks="tasks"/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
+import Tasks from './components/Tasks.vue'
 
 export default {
   name: "App",
   components: {
-    Header
+    Header,
+    Tasks
   },
   data(){
     return {
       tasks: []
     }
+  },
+  methods: {
+    deleteTask(id){
+      if (confirm('Are you sure to delete this item?')){
+        this.tasks = this.tasks.filter(task => task.id !== id)
+      }
+    },
+    toggleReminder(id){
+      this.tasks = this.tasks.map(task => task.id === id ? {...task, reminder:!task.reminder} : task)
+    }
+  },
+  created(){
+    this.tasks = [
+      {
+        "id": 1,
+        "text": "doctor appointment",
+        "day": "2021-10-18",
+        "reminder": true
+      },
+      {
+        "id": 2,
+        "text": "interview appointment",
+        "day": "2021-2-3",
+        "reminder": true
+      },
+      {
+        "id": 3,
+        "text": "meeting appointment",
+        "day": "2021-11-8",
+        "reminder": true
+      }
+    ]
   }
 };
 </script>
---
+++ src/components/Task.vue
@@ -0,0 +1,19 @@
+<template>
+    <h3 @dblclick="$emit('toggle-reminder', task.id)">{{ task.text }}</h3>
+    <p>{{ task.day }}  {{task.reminder}}</p>
+    <button @click="ondelete(task.id)">Delete</button>
+</template>
+
+<script>
+export default {
+    name: "Task",
+    props: {
+        task: Object
+    },
+    methods: {
+        ondelete(id){
+            this.$emit('delete-task', id)
+        }
+    }
+}
+</script>
